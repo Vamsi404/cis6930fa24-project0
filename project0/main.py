56814177d779df60f707e03fd589b5cacc078925
M.VAMSI <63780914+Vamsi404@users.noreply.github.com>
2024-10-01 21:47:58 -0400

    new files added

--- project0/main.py
+++ project0/main.py
@@ -46,11 +46,9 @@ def populate_database(connection, data):
 def display_status(connection):
     cursor = connection.cursor()
     cursor.execute('''SELECT nature, COUNT(*) AS event_count 
-FROM incidents 
-GROUP BY nature 
-ORDER BY nature ASC 
-LIMIT -1 OFFSET 3;
-''')
+                      FROM incidents 
+                      GROUP BY nature 
+                      ORDER BY  nature ASC''')
     records = cursor.fetchall()
     for record in records:
         print(f"{record[0]} | {record[1]}")
